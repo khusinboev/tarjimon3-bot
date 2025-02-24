1e7a6cc522d7ef585dafcee9c1dba55208d92b65
Windows 10 <adhambekhusinboyev@gmail.com>
2025-02-24 23:37:33 +0500

    first-commit

--- tests/test1.py
+++ tests/test1.py
@@ -1,3 +1,23 @@
-text = " salom "
+import sqlite3
+from pathlib import Path
 
-print(len(text.strip().split(" ")))
+def clean_html_tags():
+    conn = sqlite3.connect("../src/database/dictionary.db")
+    cursor = conn.cursor()
+
+    # eng ustunidagi barcha ma'lumotlarni olish
+    cursor.execute("SELECT rowid, uzb FROM eng_uzb")
+    rows = cursor.fetchall()
+
+    for row in rows:
+        rowid, text = row
+        if "<br>" in text:
+            cleaned_text = text.replace("<br>", " ")  # <br> ni bo'sh joy bilan almashtiramiz
+            cursor.execute("UPDATE eng_uzb SET uzb = ? WHERE rowid = ?", (cleaned_text, rowid))
+
+    conn.commit()
+    conn.close()
+    print("Baza muvaffaqiyatli yangilandi!")
+
+
+clean_html_tags()
