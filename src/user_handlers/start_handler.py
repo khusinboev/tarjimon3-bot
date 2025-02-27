e5b7d49d534fe8ce83664730391b004d5d178738
Windows 10 <adhambekhusinboyev@gmail.com>
2025-02-27 21:52:43 +0500

    first-commit

--- src/user_handlers/start_handler.py
+++ src/user_handlers/start_handler.py
@@ -17,17 +17,17 @@ async def command_start_handler(message: Message) -> None:
     # await bot.send_chat_action(chat_id=message.from_user.id, action=ChatAction.TYPING)
     sql.execute(f"""SELECT user_id FROM public.accounts WHERE user_id = {user_id}""")
     await Authenticator.auth_user(message)
-    try:
-        if await CheckData.check_on_start(message.from_user.id):
-            await message.answer(
-                text="Xush kelibsiz\n\n\nWelcome to my bot", reply_markup=await UserPanels.main_manu())
-        else:
-            await message.answer(text="Botimizdan foydalanish uchun kanalimizga azo bo'ling"
-                                      "\nSubscribe to our channel to use our bot",
-                                 reply_markup=await UserPanels.join_btn(user_id))
-    except Exception as ex:
-        await bot.forward_message(chat_id=adminStart, from_chat_id=message.chat.id, message_id=message.message_id)
-        await bot.send_message(chat_id=adminStart, text=f"Error in start: \n\n{ex}")
+    # try:
+    if await CheckData.check_on_start(message.from_user.id):
+        await message.answer(
+            text="Xush kelibsiz\n\n\nWelcome to my bot", reply_markup=await UserPanels.main_manu())
+    else:
+        await message.answer(text="Botimizdan foydalanish uchun kanalimizga azo bo'ling"
+                                  "\nSubscribe to our channel to use our bot",
+                             reply_markup=await UserPanels.join_btn(user_id))
+    # except Exception as ex:
+    #     await bot.forward_message(chat_id=adminStart, from_chat_id=message.chat.id, message_id=message.message_id)
+    #     await bot.send_message(chat_id=adminStart, text=f"Error in start: \n\n{ex}")
 
 
 @router.callback_query(lambda message: message.text == "private", lambda message: message.chat.type == ChatType.PRIVATE)
