d33dcc93ed11eececd3bbbe5297ee760ecf698e6
Adkhambek <114991635+khusinboev@users.noreply.github.com>
2025-08-15 15:14:39 +0500

    kanal qo'shishda

--- src/functions/functions.py
+++ src/functions/functions.py
@@ -1,22 +1,43 @@
+from aiogram import Bot
+from aiogram.types import CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton, User
+
 from config import sql, db, adminPanel, bot
 
 
 class CheckData:
     @staticmethod
-    async def check_on_start(user_id):
+    async def check_member(bot: Bot, user_id: int):
         sql.execute("SELECT chat_id FROM public.mandatorys")
-        rows = sql.fetchall()
-        error_code = 0
-        for row in rows:
-            r = await bot.get_chat_member(chat_id=row[0], user_id=user_id)
-            if (str(r.status) in ['member', 'creator', 'admin']) or (user_id in adminPanel):
-                pass
-            else:
-                error_code = 1
-        if error_code == 0:
-            return True
-        else:
-            return False
+        mandatory = sql.fetchall()
+        if not mandatory:
+            return True, []
+
+        channels = []
+        for chat_id in mandatory:
+            try:
+                r = await bot.get_chat_member(chat_id=chat_id[0], user_id=user_id)
+                if r.status == "left" and user_id not in adminPanel:
+                    channels.append(chat_id[0])
+                print(channels)
+            except Exception as e:
+                print(f"Xatolik: {e}")
+        return (len(channels) == 0), channels
+
+    @staticmethod
+    async def channels_btn(channels: list):
+        keyboard = []
+        for index, channel_id in enumerate(channels, 1):
+            sql.execute("SELECT username FROM public.mandatorys WHERE chat_id=%s", (channel_id,))
+            link = sql.fetchone()
+            if link:
+                keyboard.append([
+                    InlineKeyboardButton(
+                        text=f"📢 Kanal-{index}",
+                        url=link[0]
+                    )
+                ])
+        keyboard.append([InlineKeyboardButton(text="✅Qo'shildim", callback_data="check")])
+        return InlineKeyboardMarkup(inline_keyboard=keyboard)
 
 
 class PanelFunc:
@@ -32,7 +53,7 @@ class PanelFunc:
 
     @staticmethod
     async def channel_list():
-        sql.execute("SELECT chat_id from public.mandatorys")
+        sql.execute("SELECT chat_id, username from public.mandatorys")
         str = ''
         for row in sql.fetchall():
             chat_id = row[0]
@@ -40,8 +61,9 @@ class PanelFunc:
                 all_details = await bot.get_chat(chat_id=chat_id)
                 title = all_details.title
                 channel_id = all_details.id
+                channel_id = row[1]
                 info = all_details.description
-                str += f"------------------------------------------------\nKanal useri: > {chat_id}\nKamal nomi: > {title}\nKanal id si: > {channel_id}\nKanal haqida: > {info}\n"
+                str += f"------------------------------------------------\nKanal useri: > @{all_details.username}\nKamal nomi: > {title}\nKanal id si: > {channel_id}\nKanal haqida: > {info}\n"
             except Exception as e:
                 str += f"Kanalni admin qiling\n\nError: {e}"
         return str
